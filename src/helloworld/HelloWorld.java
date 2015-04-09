1304d93be5da374e15778c9da1bc7bb42bbb0733
swashyson <Gul_homer@hotmail.com>
2015-04-09 11:24:49 +0200

    Använder nu metoden för utskrift

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,7 +16,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test");
+        HelloWorld myApp = new HelloWorld();
+        
+        myApp.sayHello();
         
     }
     private void sayHello(){
